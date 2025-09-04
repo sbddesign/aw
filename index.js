30e51733da7a8a67477d6ae5defa68addd46473c
Stephen DeLorme <stephen@d.elor.me>
2025-09-04 06:19:39 +0300

    wallet and address generation

---
+++ index.js
@@ -0,0 +1,19 @@
+import { SingleKey, Wallet } from '@arkade-os/sdk'
+import crypto from 'crypto'
+
+// Generate a random private key for testing
+const privateKey = crypto.randomBytes(32).toString('hex')
+console.log('Generated private key:', privateKey)
+
+// use your private key in hex format  
+const identity = SingleKey.fromHex(privateKey)
+
+// create a wallet instance
+const wallet = await Wallet.create({
+  identity,
+  arkServerUrl: 'https://mutinynet.arkade.sh',
+})
+
+// You can receive bitcoin offchain instantly! No inbound liquidity!
+const address = await wallet.getAddress()
+console.log('Ark Address:', address)
